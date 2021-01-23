a06beb30f68ec2468c9c85d85e2f5e2781e1ef6a
Iryna Churilova <churilovairyna@gmail.com>
2021-01-23 19:15:21 +0800

    add variables chapter

---
+++ variables/name.rb
@@ -0,0 +1,9 @@
+puts "What's your first name?"
+f_name = gets.chomp
+puts "Thank you " + f_name + ". What's your last name?"
+l_name = gets.chomp
+puts "Your full name is " + f_name + " " + l_name
+
+10.times do
+    puts f_name
+end
