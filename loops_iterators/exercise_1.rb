99b04d86ce4fe42eabfba71e84d6fc310ae8f2f4
Iryna Churilova <churilovairyna@gmail.com>
2021-01-23 21:06:13 +0800

    add loops & iterators

---
+++ loops_iterators/exercise_1.rb
@@ -0,0 +1,8 @@
+x = [1, 2, 3, 4, 5]
+x.each do |a|
+  a + 1
+end
+
+puts x
+
+#prints 1, 2, 3, 4, 5 (because an array begins with 0)
---
+++ loops_iterators/exercise_2.rb
@@ -0,0 +1,7 @@
+a = ""
+while a != "STOP" do
+  puts "Hi"
+  answer = gets.chomp
+  puts "Again?"
+  a = gets.chomp
+end
